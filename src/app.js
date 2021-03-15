4442beea59f6558965456b0177f2472150aadee1
pavel <pavelv87@gmail.com>
2021-03-15 14:42:24 +0600

    test 4.6.0

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import pageLoader from './index.js';
 // const logPageLoader = debug(pageLoader);
 
 // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+const defaultPath = process.cwd();
+// console.log(defaultPath);
 
 const runApp = () => {
   // const testPath = '/home/pavel/hexlet_projects/tet';
@@ -12,13 +14,13 @@ const runApp = () => {
   // console.log(pageLoader('https://ru.hexlet.io/projects/', testPath));
   commander.version('0.0.1')
     .description('Some description')
-    .option('-out, --output [path]', 'Download path', 'process.cwd()')
+    .option('-out, --output [path]', 'Download path', defaultPath)
     .arguments('<url>')
     .action((url) => {
       return pageLoader(url, commander.output)
       .then(() => {
         console.log('all ok');
-        process.exit();
+        // process.exit();
       })
       .catch((er) => {
         console.error(er.message);

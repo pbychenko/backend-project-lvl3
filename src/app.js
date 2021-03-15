b269dc33dcdb5ce49f9fca05519c4e8139fe4ad1
pavel <pavelv87@gmail.com>
2021-03-15 12:13:57 +0600

    test 4.5

--- src/app.js
+++ src/app.js
@@ -7,17 +7,17 @@ import pageLoader from './index.js';
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const runApp = () => {
-  const testPath = '/home/pavel/hexlet_projects/tet';
-  console.log(pageLoader('https://fc-arsenal.com/pered-matchem/mikel-arteta-rodzhers-zasluzhivaet-priz-luchshemu-treneru-sezona', testPath));
+  // const testPath = '/home/pavel/hexlet_projects/tet';
+  // console.log(pageLoader('https://fc-arsenal.com/pered-matchem/mikel-arteta-rodzhers-zasluzhivaet-priz-luchshemu-treneru-sezona', testPath));
   // console.log(pageLoader('https://ru.hexlet.io/projects/', testPath));
-  // commander.version('0.0.1')
-  //   .description('Some description')
-  //   .option('-out, --output [path]', 'Download path', 'process.cwd()')
-  //   .arguments('<url>')
-  //   .action((url) => {
-  //     console.log(pageLoader(url, commander.output));
-  //   })
-  //   .parse(process.argv);
+  commander.version('0.0.1')
+    .description('Some description')
+    .option('-out, --output [path]', 'Download path', 'process.cwd()')
+    .arguments('<url>')
+    .action((url) => {
+      console.log(pageLoader(url, commander.output));
+    })
+    .parse(process.argv);
 };
 
 
